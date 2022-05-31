e4d5622c1e992172a5f61dc023095cccaf5e6379
Mauro Sanchez Matamoros <maurosanchezmatamoros@gmail.com>
2022-05-31 14:37:05 -0300

    animation about deleted

--- src/Components/Main/AboutMe.js
+++ src/Components/Main/AboutMe.js
@@ -8,12 +8,12 @@ const AboutMe = () => {
             <h2>About me</h2>
             <img src="./images/mauroHighResol.png" alt="Mauro Sanchez Matamoros" />
             <div className="AboutMe__textBox">
-                <p className="animate__animated animate__backInLeft">
+                <p>
                     Hello! Welcome to my portfolio, my name is Mauro Sanchez Matamoros, a front-end developer.
                     I'd like to tell you a little bit of me...
                     I was born in Buenos Aires, Argentina. Since then, I've always lived in Villa Lugano, a town in the south of the Capital City.
                 </p>
-                <p className="animate__animated animate__backInRight">
+                <p>
                     As regards my professional life, I've got a degree in Accountancy from the Buenos Aires University (UBA), but it was never my real passion.
                     During the pandemic, I discovered how much I like programming and, from that moment, I continued learning and practicing constantly.
                     I've completed the "Front-end React Development" program at <a href="https://www.coderhouse.com/">"Coderhouse"</a>.
