1ffd23937bccbd97bdd54c3baba4fd7784305f08
Mauro Sanchez Matamoros <maurosanchezmatamoros@gmail.com>
2022-06-10 16:35:02 -0300

    CV esp

--- src/Components/Main/CV.js
+++ src/Components/Main/CV.js
@@ -6,17 +6,32 @@ const CV = () => {
 
     const {language} = useContext(LanguageContext)
     
-    return(
+    if(language==="en"){
+        return(
         <div className="CV" >
-            <h2>{language==="en"?"My Resume":language==="es"?"Mi CV":"LanguageError"}</h2>
+            <h2>My Resume</h2>
             <div className="CV__container">
-                <a href="https://mauro-sanchez-matamoros-cv.vercel.app/pages/cvEsp.html" target="_blank" rel="noopener noreferrer">
+                <a href="https://mauro-sanchez-matamoros-cv.vercel.app" target="_blank" rel="noopener noreferrer">
                     <img className="CV__inside" src="./images/cv-mauro-sanchez-matamoros.png" alt="cv mauro sanchez matamoros"/>
                 </a>
             </div>
             <img className="scrollDown animate__animated animate__fadeInDown" src="./svg/arrowDownBlack.svg" alt="arrow down"/>
             <a href="./images/cv-mauro-sanchez-matamoros.pdf" download={"CV Mauro Sanchez Matamoros"}><img className="CV__svg" src="./svg/download.svg" alt="download"/></a>
-            <span>{language==="en"?"Download":language==="es"?"Descargar":"LanguageError"}</span>
+            <span>Download</span>
+        </div>
+    )}
+
+    return(
+        <div className="CV" >
+            <h2>Mi CV</h2>
+            <div className="CV__container">
+                <a href="https://mauro-sanchez-matamoros-cv.vercel.app/pages/cvEsp.html" target="_blank" rel="noopener noreferrer">
+                    <img className="CV__inside" src="./images/cv-mauro-sanchez-matamoros-es.png" alt="cv mauro sanchez matamoros"/>
+                </a>
+            </div>
+            <img className="scrollDown animate__animated animate__fadeInDown" src="./svg/arrowDownBlack.svg" alt="arrow down"/>
+            <a href="./images/cv-mauro-sanchez-matamoros-es.pdf" download={"CV Mauro Sanchez Matamoros"}><img className="CV__svg" src="./svg/download.svg" alt="download"/></a>
+            <span>Descargar</span>
         </div>
     )
 }
